9552207c11c908bf976e87e1fafd54550f23ae72
schleising <stephen@schleising.net>
2024-05-19 11:23:40 +0100

    Improve file handling and output

--- python/porter-stemmer.py
+++ python/porter-stemmer.py
@@ -353,20 +353,21 @@ if __name__ == '__main__':
     p = PorterStemmer()
     if len(sys.argv) > 1:
         for f in sys.argv[1:]:
-            infile = open(f, 'r')
-            while True:
-                output = ''
-                word = ''
-                line = infile.readline()
-                if line == '':
-                    break
-                for c in line:
-                    if c.isalpha():
-                        word += c.lower()
-                    else:
-                        if word:
-                            output += p.stem(word, 0,len(word)-1)
-                            word = ''
-                        output += c.lower()
-                print(output.strip())
-            infile.close()
+            with open(f, 'r') as infile:
+                while True:
+                    output = ''
+                    input = ''
+                    word = ''
+                    line = infile.readline()
+                    if line == '':
+                        break
+                    for c in line:
+                        if c.isalpha():
+                            word += c.lower()
+                        else:
+                            if word:
+                                output += p.stem(word, 0,len(word)-1)
+                                input = word
+                                word = ''
+                            output += c.lower()
+                    print(f'{input:15}: {output.strip()}')
